9b5021776fac93cd768d692d91da563f35e9f5b1
TecnoGroup <37882432+TecnoGroup@users.noreply.github.com>
2018-03-28 17:01:24 -0300

    Add files via upload

---
+++ public_html/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>TecnoGroup Solutions</title>
+<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="js/custom.js"></script>
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"> </script>
+<script src="https://cdn.rawgit.com/nnattawat/flip/master/dist/jquery.flip.min.js"></script>
+
+<link rel="stylesheet" type="text/css" href="css/style.css"/>
+</head>
+    <div id="cabecera">
+        <img src="images/logo.jpg" id="img"/>
+    </div>
+<body>
+    <div style="background-color:#FFF;height:10px;opacity:0.7;width:1330px"></div>
+	<div id="menu">
+    <ul id="menuu">
+        <li><a href="#" title="Home"><span>Home</span></a></li>
+        <li><a href="#" title="Servicios"><span>Servicios</span></a></li>
+        <li><a href="#" title="Nosotros"><span>Nosotros</span></a></li>
+    </ul>
+    </div>
+    <div id="prol">
+        <div align="center" style="font-size:18px">OBJETIVO
+            <p>
+            El objetivo de nuestro emprendimiento es proporcionar servicios de calidad para lograr mayor difusiòn y facilidad de negocio a nuestros clientes.
+            </p>
+        </div>
+        <div align="center" style="font-size:18px">MISION
+            <p>
+            Establecer un compromiso con el cliente garantizando su completa satisfaccion.
+            </p>
+        </div>
+        <div align="center" style="font-size:18px">VISION
+            <p>
+            Establecer un compromiso con el cliente garantizando su completa satisfaccion.
+            </p>
+        </div>
+    </div>
+    <div class="container" align="center">
+       	<div class="row">
+            <div class="col-xs-3 col-sm-4 col-md-5">
+                <div class="card"> 
+                  <div class="front"> 
+                   	<img src="images/diseñograf400x300.jpg" />
+                  </div> 
+                  <div class="back">
+                    Back content
+                  </div> 
+                </div>
+            </div>
+			<div class="col-xs-3 col-sm-4 col-md-5">
+                <div class="card"> 
+                  <div class="front"> 
+                   	<img src="images/diseñograf400x300.jpg" />
+                  </div> 
+                  <div class="back">
+                    Back content
+                  </div> 
+                </div>
+			</div>
+        </div>
+    </div>
+</body>
+<footer>
+
+</footer>
+</html>
