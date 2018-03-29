c0e71ce9a69ad9e3cb87ff79f8cd26d6f4c3c197
TecnoGroup <37882432+TecnoGroup@users.noreply.github.com>
2018-03-28 22:15:25 -0300

    Add files via upload

--- public_html/index.php
+++ public_html/index.php
@@ -3,27 +3,20 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>TecnoGroup Solutions</title>
+<link rel="stylesheet" type="text/css" href="css/style.css"/>
 <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
-<script type="text/javascript" src="js/custom.js"></script>
 <script type="text/javascript" src="js/flip.js"></script>
 <script type="text/javascript" src="js/jquery.flip.js"></script>
 <script type="text/javascript" src="js/jquery.flip.min.js"></script>
-<script>
-$(document).ready(function(){
-$(".card").flip({
-  axis: 'y',
-  trigger: 'hover'
-});
-});
-</script>
+<script type="text/javascript" src="js/custom.js"></script>
 
-<link rel="stylesheet" type="text/css" href="css/style.css"/>
 </head>
     <div id="cabecera">
         <img src="images/logo.jpg" id="img"/>
     </div>
 <body>
-    <div style="background-color:#FFF;height:10px;opacity:0.7;width:1330px"></div>
+    <div id="menu"></div>
+    <!--
 	<div id="menu">
     <ul id="menuu">
         <li><a href="#" title="Home"><span>Home</span></a></li>
@@ -31,8 +24,9 @@ $(".card").flip({
         <li><a href="#" title="Nosotros"><span>Nosotros</span></a></li>
     </ul>
     </div>
-    <div id="prol">
-        <div align="center" style="font-size:18px">OBJETIVO
+    -->
+    <div>
+        <div align="center" style="font-size:18px;margin-top:20px">OBJETIVO
             <p>
             El objetivo de nuestro emprendimiento es proporcionar servicios de calidad para lograr mayor difusiòn y facilidad de negocio a nuestros clientes.
             </p>
@@ -48,30 +42,61 @@ $(".card").flip({
             </p>
         </div>
     </div>
-    <div class="container" align="center">
+    <div class="container">
+    <div class="titulo">SERVICIOS</div>
        	<div class="row">
-            <div class="col-xs-3 col-sm-4 col-md-5">
+            <div class="col-xs-1 col-sm-4 col-md-2">
                 <div class="card"> 
                   <div class="front"> 
-                   	<img src="images/diseñograf400x300.jpg" />
+                   	<img src="images/diseño.png" />
+                  </div> 
+                  <div class="back">
+                    Diseño Grafico
+                  </div> 
+                </div>
+            </div>
+			<div class="col-xs-1 col-sm-4 col-md-2">
+                <div class="card"> 
+                  <div class="front"> 
+                   	<img src="images/apps.png" />
+                  </div> 
+                  <div class="back">
+                    Aplicaciones Web
+                  </div> 
+                </div>
+			</div>
+            <div class="col-xs-1 col-sm-4 col-md-2">
+                <div class="card"> 
+                  <div class="front"> 
+                    <img src="images/desktop.png" />
+                  </div> 
+                  <div class="back">
+                    Aplicaciones de Escritorio
+                  </div> 
+                </div>
+            </div>
+            <div class="col-xs-1 col-sm-4 col-md-2">
+                <div class="card"> 
+                  <div class="front"> 
+                   	<img src="images/consultant.png" />
+                  </div> 
+                  <div class="back">
+                  Asesoria
+                  </div> 
+                </div>
+            </div>
+            <div class="col-xs-1 col-sm-4 col-md-2">
+                <div class="card"> 
+                  <div class="front"> 
+                   	<img src="images/diseño.png" />
                   </div> 
                   <div class="back">
                     Back content
                   </div> 
                 </div>
             </div>
-			<div class="col-xs-3 col-sm-4 col-md-5">
-                <div class="card"> 
-                  <div class="front"> 
-                   	<img src="images/diseñograf400x300.jpg" />
-                  </div> 
-                  <div class="back">
-                    Back content
-                  </div> 
-                </div>
-			</div>
-        </div>
-    </div>
+        </div>          
+     </div>
 </body>
 <footer>
 
--- public_html/js/custom.js
+++ public_html/js/custom.js
@@ -1,4 +1,5 @@
 //funciones para cambiar el color del menu al pasar el mouse sobre un item de la lista//
+
 $(document).ready(function(){
 	$(".lista1").hover(function(){
 		$(".lista1").css("color", "#FFF");
@@ -20,96 +21,10 @@ $(document).ready(function(){
 			$(".lista3").css("color", "#CCC");
 		});
 });
-//funcion para guirar una imagen//
+
 $(document).ready(function(){
-$(".card").flip({
-  axis: 'y',
-  trigger: 'hover'
+	$(".card").flip({
+	  axis: 'y',
+	  trigger: 'hover'
+	});
 });
-});
-
-// NIVO LIGHTBOX
-$('.iso-box-section a').nivoLightbox({
-        effect: 'fadeScale',
-    });
-
-// ISOTOPE FILTER
-jQuery(document).ready(function($){
-
-	if ( $('.iso-box-wrapper').length > 0 ) { 
-
-	    var $container 	= $('.iso-box-wrapper'), 
-	    	$imgs 		= $('.iso-box img');
-
-
-
-	    $container.imagesLoaded(function () {
-
-	    	$container.isotope({
-				layoutMode: 'fitRows',
-				itemSelector: '.iso-box'
-	    	});
-
-	    	$imgs.load(function(){
-	    		$container.isotope('reLayout');
-	    	})
-
-	    });
-
-	    //filter items on button click
-
-	    $('.filter-wrapper li a').click(function(){
-
-	        var $this = $(this), filterValue = $this.attr('data-filter');
-
-			$container.isotope({ 
-				filter: filterValue,
-				animationOptions: { 
-				    duration: 750, 
-				    easing: 'linear', 
-				    queue: false, 
-				}              	 
-			});	            
-
-			// don't proceed if already selected 
-
-			if ( $this.hasClass('selected') ) { 
-				return false; 
-			}
-
-			var filter_wrapper = $this.closest('.filter-wrapper');
-			filter_wrapper.find('.selected').removeClass('selected');
-			$this.addClass('selected');
-
-	      return false;
-	    }); 
-
-	}
-
-});
-
-
-// HIDE MOBILE MENU AFTER CLIKING ON A LINK
-   $('.navbar-collapse a').click(function(){
-        $(".navbar-collapse").collapse('hide');
-    });
-
-
-// SCROLLTO THE TOP
-$(document).ready(function() {
-	// Show or hide the sticky footer button
-		$(window).scroll(function() {
-			if ($(this).scrollTop() > 200) {
-				$('.go-top').fadeIn(200);
-					} else {
-						$('.go-top').fadeOut(200);
-					}
-				});		
-				// Animate the scroll to top
-				$('.go-top').click(function(event) {
-					event.preventDefault();
-				
-					$('html, body').animate({scrollTop: 0}, 300);
-				})
-			});
-			
